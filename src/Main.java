7767a8f363a0d382bd9d2cd8f0e4ceeb465607cf
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2023-05-14 23:54:35 +0200

    O(n) insert and delete

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //MAX STRING LENGTH IS 10
+        // MAX STRING LENGTH IS 10
 
         try {
             List<String> lines = Files.readAllLines(Paths.get("src/Test.txt"));
@@ -17,15 +17,14 @@ public class Main {
             ON2 h = new ON2(hash);
             h.print();
 
-            //insert one element
+            // insert one element
             h.insert("raffiq");
 
-
-            //search
+            // search
             h.lookUp("ahmed");
             h.lookUp("anything");
 
-            //batch insert
+            // batch insert
             List<String> lines2 = Files.readAllLines(Paths.get("src/Test2.txt"));
             String[] array2 = lines2.toArray(new String[0]);
             array2 = Arrays.stream(array2).distinct().toArray(String[]::new);
@@ -34,17 +33,18 @@ public class Main {
             h = new ON2(hash);
             h.print();
 
-            //delete
+            // delete
             System.out.println(h.delete("ahmed"));
-            System.out.println(h.delete("anything"));;
+            System.out.println(h.delete("anything"));
+            ;
 
-            //batch delete
+            // batch delete
             List<String> lines3 = Files.readAllLines(Paths.get("src/Test2.txt"));
             String[] array3 = lines3.toArray(new String[0]);
             array3 = Arrays.stream(array3).distinct().toArray(String[]::new);
             int count = 0;
-            for(int i = 0; i < array3.length; i++){
-                if(h.delete(array3[i])){
+            for (int i = 0; i < array3.length; i++) {
+                if (h.delete(array3[i])) {
                     count++;
                 }
             }
@@ -52,87 +52,132 @@ public class Main {
             System.out.println("Deleted " + count + " Strings");
             h.print();
 
-
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        ////// O(n) start
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("src/Test.txt"));
+            String[] array = lines.toArray(new String[0]);
+            array = Arrays.stream(array).distinct().toArray(String[]::new);
+            Hashing hash = new Hashing(array);
 
+            ON h = new ON(hash);
+
+            h.print();
+
+            // insert one element
+            System.out.println(h.insert("raffiq"));
+
+            // search
+            System.out.println(h.search("ahmed"));
+            System.out.println(h.search("anything"));
+
+            // batch insert
+            List<String> lines2 = Files.readAllLines(Paths.get("src/Test2.txt"));
+            String[] array2 = lines2.toArray(new String[0]);
+            array2 = Arrays.stream(array2).distinct().toArray(String[]::new);
+            hash.batchinsert(array2);
+            h = null;
+            h = new ON(hash);
+            h.print();
+
+            // delete
+            System.out.println(h.delete("ahmed"));
+            System.out.println(h.delete("anything"));
+            ;
+
+            // batch delete
+            List<String> lines3 = Files.readAllLines(Paths.get("src/Test2.txt"));
+            String[] array3 = lines3.toArray(new String[0]);
+            array3 = Arrays.stream(array3).distinct().toArray(String[]::new);
+            int count = 0;
+            for (int i = 0; i < array3.length; i++) {
+                if (h.delete(array3[i])) {
+                    count++;
+                }
+            }
+            hash = new Hashing(array3);
+            h = new ON(hash);
+            System.out.println("Deleted " + count + " Strings");
+            h.print();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ////// O(n) ends
     }
 
+    // boolean valid = false;
+    // while (!valid) {
+    // System.out.println("Choose Method 1 for ON2 2 for ON");
+    // Scanner sc = new Scanner(System.in);
+    // int input = sc.nextInt();
+    // switch (input) {
+    // case 1:
+    // valid = true;
+    // ON2 h = new ON2(hash);
+    // h.print();
+    // System.out.println("1 for delete, 2 for insert, 3 for batch insert, 4 for
+    // search");
+    // int fun = sc.nextInt();
+    // switch (fun) {
+    // case 1:{
+    // System.out.println("What do u want to delete?");
+    // String in = sc.nextLine();
+    // h.delete(in);
+    // }
+    //
+    // case 2:{
+    // System.out.println("Enter string to insert");
+    // String toInsert = sc.nextLine();
+    // h.insert(toInsert);
+    // h.print();}
+    //
+    // case 3:{
+    // System.out.println("Give path of file");
+    // String path = sc.nextLine();}
+    //
+    // default:
+    //
+    // }
+    //
+    // case 2:
+    // System.out.println("Apply ON");
+    // valid = true;
+    //
+    // default:
+    // System.out.println("Invalid Choice");
+    // valid = false;
+    // }
+    // }
 
-//           boolean valid = false;
-//            while (!valid) {
-//                System.out.println("Choose Method 1 for ON2 2 for ON");
-//                Scanner sc = new Scanner(System.in);
-//                int input = sc.nextInt();
-//                switch (input) {
-//                    case 1:
-//                        valid = true;
-//                        ON2 h = new ON2(hash);
-//                        h.print();
-//                        System.out.println("1 for delete, 2 for insert, 3 for batch insert, 4 for search");
-//                        int fun = sc.nextInt();
-//                        switch (fun) {
-//                            case 1:{
-//                                System.out.println("What do u want to delete?");
-//                                String in = sc.nextLine();
-//                                h.delete(in);
-//                               }
-//
-//                            case 2:{
-//                                System.out.println("Enter string to insert");
-//                                String toInsert = sc.nextLine();
-//                                h.insert(toInsert);
-//                                h.print();}
-//
-//                            case 3:{
-//                                System.out.println("Give path of file");
-//                                String path = sc.nextLine();}
-//
-//                            default:
-//
-//                        }
-//
-//                    case 2:
-//                        System.out.println("Apply ON");
-//                        valid = true;
-//
-//                    default:
-//                        System.out.println("Invalid Choice");
-//                        valid = false;
-//                }
-//            }
-
-
-
-
-//        //test case
-//        String[] s = {"helloooo", "hello", "malo", "malosh", "zamalek", "eren", "yeager", "zamalek", "ahmed"};
-//
-//        //get an array without repeated strings
-//        s = Arrays.stream(s).distinct().toArray(String[]::new);
-//
-//
-//        Hashing hash = new Hashing(s);
-////        //insert
-////        hash.insert(s);
-//        ON2 h = new ON2(hash);
-//        h.print();
-//        System.out.println("Enter key to search for.. ");
-//        Scanner sc = new Scanner(System.in);
-//        String input = sc.nextLine();
-//        h.lookUp(input);
-//        System.out.println("Delete something");
-//        String input2 = sc.nextLine();
-//        h.delete(input2);
-//        System.out.println("insert again");
-//        String[] s2 = {"heo", "ello"};
-//        hash.batchinsert(s2);
-//        //ON2 h2 = new ON2(hash);
-//        h = null;
-//        h = new ON2(hash);
-//        h.print();
-    }
+    // //test case
+    // String[] s = {"helloooo", "hello", "malo", "malosh", "zamalek", "eren",
+    // "yeager", "zamalek", "ahmed"};
+    //
+    // //get an array without repeated strings
+    // s = Arrays.stream(s).distinct().toArray(String[]::new);
+    //
+    //
+    // Hashing hash = new Hashing(s);
+    //// //insert
+    //// hash.insert(s);
+    // ON2 h = new ON2(hash);
+    // h.print();
+    // System.out.println("Enter key to search for.. ");
+    // Scanner sc = new Scanner(System.in);
+    // String input = sc.nextLine();
+    // h.lookUp(input);
+    // System.out.println("Delete something");
+    // String input2 = sc.nextLine();
+    // h.delete(input2);
+    // System.out.println("insert again");
+    // String[] s2 = {"heo", "ello"};
+    // hash.batchinsert(s2);
+    // //ON2 h2 = new ON2(hash);
+    // h = null;
+    // h = new ON2(hash);
+    // h.print();
+}
--- src/ON.java
+++ src/ON.java
@@ -1,28 +1,42 @@
 import java.util.ArrayList;
 
 public class ON {
-    public int[][] result;
+    public String[][] result;
     private int[][] h1fun;
     private int[][][] h2funs;
     private int n;
     private int notFound = 0;
     private int alreadyExists = 0;
     private Hashing hash;
+    private ArrayList<String>[] level1;
+    public int order;
 
     public ON(Hashing h) {
         hash = h;
         this.n = hash.n;
-        h1fun = hash.randomH(this.n);
-        ArrayList<String>[] chain = constructLevel1(); // linked list is needed due to collision of level1
-        constructLevel2(chain);
+        sequance();
+    }
+
+    private void sequance() {
+        hashFunction();
+        while (!acceptable())
+            hashFunction();
+        constructLevel2();
+    }
+
+    private void hashFunction() {
+        h1fun = hash.randomH((int) Math.floor(Math.log(n) / Math.log(2)));
+        constructLevel1(); // linked list is needed due to collision of level1
     }
 
     // make the level 1 hashing by putting each element in its place with collsion
     private ArrayList<String>[] constructLevel1() {
-        ArrayList<String>[] level1 = new ArrayList[this.n];
+        level1 = new ArrayList[this.n];
         for (int i = 0; i < this.n; i++) {
-            int[] hashBin = hash.multiply(h1fun, hash.S[i]);
-            int index = hash.convertToDecimal(hashBin);
+            int index = hash.hashCode(hash.S[i], h1fun);
+            if (level1[index] == null) {
+                level1[index] = new ArrayList<String>();
+            }
             if (level1[index].contains(hash.S[i]))
                 alreadyExists++;
             else
@@ -31,10 +45,30 @@ public class ON {
         return level1;
     }
 
+    // check that level 1 is acceptable
+    private boolean acceptable() {
+        order = n;
+        int count = 0;
+        for (int i = 0; i < level1.length; i++) {
+            if (level1[i] != null) {
+                count += (int) Math.pow(level1[i].size(), 2);
+                order += level1[i].size();
+            }
+
+        }
+        if (count >= 2 * n)
+            return false;
+        else {
+            return true;
+        }
+    }
+
     // make the level 2 hashing by making O(n^2) hashing for each cell
-    private void constructLevel2(ArrayList<String>[] level1) {
+    private void constructLevel2() {
+        h2funs = new int[n][][];
+        result = new String[n][];
         for (int i = 0; i < this.n; i++) {
-            if (level1[i].size() != 0) {
+            if (level1[i] != null) {
                 Hashing Hi = new Hashing(toArray(level1[i]));
                 ON2 sec = new ON2(Hi);
                 h2funs[i] = sec.H;
@@ -56,14 +90,14 @@ public class ON {
     }
 
     // delete
-    ////////////////////////////// need edit
     public boolean delete(String s) {
         int i = hash.hashCode(s, h1fun);
-        if (h2funs[i].length != 0) {
+        if (h2funs[i] != null && h2funs[i].length != 0) {
             int j = hash.hashCode(s, h2funs[i]);
             if (s.equals(result[i][j])) {
-                ////////////// change 0 to ""
-                result[i][j] = 0;
+                result[i][j] = null;
+                hash.S = remove(hash.S, s);
+                this.n--;
                 return true;
             }
         }
@@ -71,7 +105,40 @@ public class ON {
     }
 
     // insert
-    public void insert(String s) {
+    public boolean insert(String s) {
+        int i = hash.hashCode(s, h1fun);
+        // there is a collision in level 1
+        if (h2funs[i] != null && h2funs[i].length != 0) {
+            int j = hash.hashCode(s, h2funs[i]);
+            // level 2 is ready just insert
+            if (result[i][j] == null) {
+                hash.S = concat(hash.S, s);
+                level1[i].add(s);
+                if (!acceptable()) {
+                    sequance();
+                } else
+                    result[i][j] = s;
+                return true;
+            }
+            // already exists
+            else if (result[i][j].equals(s))
+                return false;
+            // need rehash the same cell has another word
+            else {
+                hash.S = concat(hash.S, s);
+                sequance();
+                return true;
+            }
+        } else {
+            hash.S = concat(hash.S, s);
+            String[] temp = new String[1];
+            temp[0] = s;
+            Hashing Hi = new Hashing(temp);
+            ON2 sec = new ON2(Hi);
+            h2funs[i] = sec.H;
+            result[i] = sec.result;
+            return true;
+        }
 
     }
 
@@ -92,4 +159,38 @@ public class ON {
             ans[i] = level1Elems.get(i);
         return ans;
     }
+
+    private String[] concat(String[] old, String s) {
+        String[] newArr = new String[old.length + 1];
+        for (int i = 0; i < old.length; i++) {
+            newArr[i] = old[i];
+        }
+        newArr[old.length] = s;
+        return newArr;
+    }
+
+    private String[] remove(String[] old, String s) {
+        String[] newArr = new String[old.length - 1];
+        int count = 0;
+        for (int i = 0; i < old.length; i++) {
+            if (!old[i].equals(s))
+                newArr[count++] = old[i];
+        }
+        return newArr;
+    }
+
+    public void print() {
+        for (int i = 0; i < n; i++) {
+            if (result[i] != null) {
+                for (int j = 0; j < result[i].length; j++) {
+                    if (result[i][j] != null)
+                        System.out.print(result[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+        System.out.println("order is " + order + " size is " + n);
+    }
+
 }
