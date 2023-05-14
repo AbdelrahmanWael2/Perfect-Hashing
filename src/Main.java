f6b9168f8b5ffdf40de8ca1c3705a57abe1cb409
A.Wael <96313471+AbdelrahmanWael2@users.noreply.github.com>
2023-05-14 19:22:01 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,21 @@ public class Main {
 
 
         Hashing hash = new Hashing(s);
+//        //insert
+//        hash.insert(s);
         ON2 h = new ON2(hash);
         h.print();
         System.out.println("Enter key to search for.. ");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         h.lookUp(input);
-
+        System.out.println("Delete something");
+        String input2 = sc.nextLine();
+        h.delete(input2);
+        System.out.println("insert again");
+        String[] s2 = {"heo", "ello"};
+        hash.insert(s2);
+        ON2 h2 = new ON2(hash);
+        h2.print();
     }
 }
