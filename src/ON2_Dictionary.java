47ae1461ffff593d1209ed03af41b8180364ae62
A.Wael <96313471+AbdelrahmanWael2@users.noreply.github.com>
2023-05-20 23:26:59 +0300

    Tested ON2

--- src/ON2_Dictionary.java
+++ src/ON2_Dictionary.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,6 +23,7 @@ public class ON2_Dictionary implements Dictionary {
         // On2_dictionary = new ON2(h);
         On2_dictionary.print();
         // return insertString + " inserted successfully";
+        System.out.println("Size = " + countLines());
         return "";
     }
 
@@ -34,6 +37,7 @@ public class ON2_Dictionary implements Dictionary {
         if (On2_dictionary.delete(deleteString)) {
             // On2_dictionary = new ON2(h);
             On2_dictionary.print();
+            System.out.println("Size = " + countLines());
             System.out.println(deleteString + " was found and deleted");
             return true;
         } else {
@@ -61,7 +65,7 @@ public class ON2_Dictionary implements Dictionary {
         }
         On2_dictionary = new ON2(h);
         On2_dictionary.print();
-
+        System.out.println("Size = " + countLines());
         return "Batch insertion finished, Total Number of elements= " + size;
     }
 
@@ -83,6 +87,26 @@ public class ON2_Dictionary implements Dictionary {
         }
         // On2_dictionary = new ON2(h);
         On2_dictionary.print();
+        System.out.println("Size = " + countLines());
         return "Batch deletion finished, Deleted: " + deletions;
     }
+
+
+
+
+    public static int countLines() {
+        int lineCount = 0;
+        String filePath = "src/Output.txt";
+        System.out.println("Number of lines in the file: " + lineCount);
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while (reader.readLine() != null) {
+                lineCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lineCount;
+    }
 }
+
+
